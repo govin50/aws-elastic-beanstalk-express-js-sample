e7bdab46b4fb1e49ad82f3d7bdfa2c755c090970
Govin <gopalgbnd@gmail.com>
2025-10-04 18:29:04 +1000

    changing package.json

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 8080;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+const port = process.env.PORT || 8080;
+app.get('/', (_req, res) => res.send('Hello from Assignment 2!'));
+app.listen(port, () => console.log(`Listening on ${port}`));
